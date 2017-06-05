3109b177dc74d98a47678d85ea5063627f5ccacc
ace <azatibrakov@gmail.com>
2017-06-05 22:47:00 +0700

    Solution for ninth problem

---
+++ 9.py
@@ -0,0 +1,43 @@
+from itertools import permutations
+from math import sqrt
+from typing import Tuple
+
+from utils import product
+
+MAX_NUMBER = 1000
+
+MIN_N = 1
+MIN_M = 2
+
+
+def pythagorean_triples_candidates(*,
+                                   stop: int):
+    # based on
+    # https://en.wikipedia.org/wiki/Pythagorean_triple#Generating_a_triple
+    max_k = stop // (MIN_N ** 2 + MIN_M ** 2)
+    for k in range(1, max_k + 1):
+        max_m = int(sqrt(stop // k))
+        numbers = range(1, max_m)
+        for n, m in map(sorted, permutations(numbers, r=2)):
+            candidate = sorted([m ** 2 - n ** 2,
+                                2 * m * n,
+                                m ** 2 + n ** 2])
+            yield tuple(k * coordinate
+                        for coordinate in candidate)
+
+
+def is_pythagorean_triple(numbers: Tuple[int, int, int]) -> bool:
+    a, b, c = numbers
+    return a ** 2 + b ** 2 == c ** 2
+
+
+def special_condition(numbers: Tuple[int, ...]) -> bool:
+    return sum(numbers) == MAX_NUMBER
+
+
+special_pythagorean_triplet, = filter(is_pythagorean_triple,
+                                      filter(special_condition,
+                                             set(pythagorean_triples_candidates(
+                                                 stop=MAX_NUMBER))))
+
+assert product(special_pythagorean_triplet) == 31_875_000
