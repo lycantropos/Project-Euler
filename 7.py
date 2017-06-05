3de9f23622bc74b2220ee5438c3b43bf49ef1141
ace <azatibrakov@gmail.com>
2017-06-05 17:55:27 +0700

    Solution for seventh problem

---
+++ 7.py
@@ -0,0 +1,26 @@
+from itertools import (count,
+                       islice)
+from math import sqrt
+
+
+def is_prime(number: int) -> bool:
+    if number == 2:
+        return True
+    if number == 1 or number % 2 == 0:
+        return False
+    stop = int(sqrt(number))
+    odd_factors = range(3, stop + 1, 2)
+    for factor in odd_factors:
+        if number % factor == 0:
+            return False
+    return True
+
+
+def prime_number_by_index(index: int) -> int:
+    prime_numbers = filter(is_prime, count(1))
+    res, = islice(prime_numbers, index - 1, index)
+    return res
+
+
+assert prime_number_by_index(6) == 13
+assert prime_number_by_index(10_001) == 104_743
