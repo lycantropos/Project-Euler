9c50108027ab1e32272f6f749065331f2e87dbe5
ace <azatibrakov@gmail.com>
2017-06-07 10:37:55 +0700

    Solution for twenty fifth problem

---
+++ 25.py
@@ -0,0 +1,16 @@
+from utils import fibonacci
+
+
+def n_digits_fibonacci_number_index(digits_count: int) -> int:
+    def has_n_digits(number: int) -> bool:
+        return len(str(number)) >= digits_count
+
+    return next(index
+                for index, number in enumerate(fibonacci(), start=1)
+                if has_n_digits(number))
+
+
+assert n_digits_fibonacci_number_index(1) == 1
+assert n_digits_fibonacci_number_index(2) == 7
+assert n_digits_fibonacci_number_index(3) == 12
+assert n_digits_fibonacci_number_index(1_000) == 4_782
