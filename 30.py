f96838ea2dca651b0feaa753ebbeb3810ea82d6c
ace <azatibrakov@gmail.com>
2017-06-13 16:03:08 +0700

    Solution for thirtieth problem

---
+++ 30.py
@@ -0,0 +1,35 @@
+from functools import partial
+from itertools import count, filterfalse
+from typing import Iterable
+
+from utils import digits
+
+
+def exists_digits_powers_sum_number(digits_count: int,
+                                    *,
+                                    exponent: int):
+    return 9 ** (exponent + 1) * digits_count >= 10 ** digits_count - 1
+
+
+def max_digits_powers_sum_digits_count(exponent: int) -> int:
+    return next(filterfalse(partial(exists_digits_powers_sum_number,
+                                    exponent=exponent),
+                            count(1)))
+
+
+def digit_powers(*,
+                 exponent: int,
+                 step: int = 1) -> Iterable[int]:
+    def is_digits_powers_sum(number: int) -> bool:
+        return sum(map(power, digits(number))) == number
+
+    def power(number: int) -> int:
+        return number ** exponent
+
+    digits_count = max_digits_powers_sum_digits_count(exponent)
+    stop = 10 ** digits_count
+    yield from filter(is_digits_powers_sum, range(2, stop, step))
+
+
+assert sum(digit_powers(exponent=4)) == 19_316
+assert sum(digit_powers(exponent=5)) == 443_839
