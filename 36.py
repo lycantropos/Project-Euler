e1671177a5e183d7452b0d3f63631460f01bee6a
ace <azatibrakov@gmail.com>
2017-06-16 02:11:44 +0700

    Solution for thirty sixth problem

---
+++ 36.py
@@ -0,0 +1,24 @@
+from typing import Iterable
+
+from utils import is_palindrome
+
+
+def double_base_palindromes(*,
+                            start: int,
+                            stop: int,
+                            step: int = 1) -> Iterable[int]:
+    numbers = range(start, stop, step)
+    yield from filter(is_double_based_palindrome, numbers)
+
+
+def is_double_based_palindrome(number: int) -> bool:
+    return is_palindrome(str(number)) and is_palindrome(to_binary(number))
+
+
+def to_binary(number: int) -> str:
+    return f'{number:b}'
+
+
+assert is_double_based_palindrome(585)
+assert sum(double_base_palindromes(start=0,
+                                   stop=1_000_000)) == 872_187
