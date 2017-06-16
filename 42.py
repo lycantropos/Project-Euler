f1a72da3583efaf22f287765b58fc753bfcdef6a
ace <azatibrakov@gmail.com>
2017-06-16 13:08:28 +0700

    Solution for forty second problem

---
+++ 42.py
@@ -0,0 +1,23 @@
+from string import ascii_uppercase
+
+from utils import (parse_lines,
+                   triangular)
+
+letters_positions = {letter: position
+                     for position, letter in enumerate(ascii_uppercase,
+                                                       start=1)}
+
+
+def triangular_word(word: str) -> bool:
+    word_value = sum(letters_positions[letter]
+                     for letter in word)
+    return triangular(word_value)
+
+
+with open('words.txt') as words_file:
+    words = list(parse_lines(words_file))
+
+triangular_words = filter(triangular_word, words)
+
+assert triangular_word('SKY')
+assert sum(1 for _ in triangular_words) == 162
