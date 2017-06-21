ac0edff7fe9d9daacfbcf0243a6007c63271e70a
ace <azatibrakov@gmail.com>
2017-06-21 19:56:02 +0700

    Solution for fifty seventh problem

---
+++ 57.py
@@ -0,0 +1,26 @@
+from fractions import Fraction
+from typing import Iterable
+
+from utils import (number_to_digits,
+                   capacity)
+
+
+def square_root_expansions(stop: int) -> Iterable[Fraction]:
+    result = Fraction(3, 2)
+    yield result
+    for index in range(stop):
+        result = Fraction(1) + Fraction(1, 1 + result)
+        yield result
+
+
+def square_root_convergents(*,
+                            stop: int) -> Iterable[bool]:
+    for expansion in square_root_expansions(stop):
+        numerator_digits_count = capacity(number_to_digits(expansion
+                                                           .numerator))
+        denominator_digits_count = capacity(number_to_digits(expansion
+                                                             .denominator))
+        yield numerator_digits_count > denominator_digits_count
+
+
+assert sum(square_root_convergents(stop=1_001)) == 153
