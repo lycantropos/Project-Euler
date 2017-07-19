cdc6bd0311948b838fb1e11010ddc60dece2865b
ace <azatibrakov@gmail.com>
2017-07-19 12:18:52 +0700

    Solution for seventieth problem

---
+++ 70.py
@@ -0,0 +1,20 @@
+from typing import Iterable
+
+from utils import (phi,
+                   n_phi)
+
+
+def are_permutations(iterable: Iterable,
+                     other_iterable: Iterable) -> bool:
+    return sorted(iterable) == sorted(other_iterable)
+
+
+def totient_permutations(numbers: Iterable[int]) -> Iterable[int]:
+    totients = map(phi, numbers)
+    for number, totient in zip(numbers, totients):
+        if are_permutations(str(number), str(totient)):
+            yield number
+
+
+assert min(totient_permutations(range(3, 10_000_000)),
+           key=n_phi) == 8_319_823
